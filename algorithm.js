1c352180686630f1cac7a385344b86daf9e6e1c4
Daniel Thompson <danthwa@gmail.com>
2017-06-06 19:13:07 -0700

    algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,15 @@
+
+
+
+function myFunc(argument){
+    var string = ""
+    string += argument
+
+    return string
+
+    console.log("something here")
+}
+
+
+var x = myFunc("Argument")
+console.log(x)
